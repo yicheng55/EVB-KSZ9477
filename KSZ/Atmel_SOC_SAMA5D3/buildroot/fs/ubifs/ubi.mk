ef52b1063a6bbe9d8e1ba3c8824b9e8349b87ab5
yi-cheng <l5977951@gmail.com>
2025-12-16 18:28:05 +0800

    chore: 更新 ubifs 配置檔案路徑及清理流程
    Detailed changes:
    - 修正 UBINIZE_CONFIG_FILE_PATH 的路徑為 $(TOPDIR)/fs/ubifs/ubinize.cfg
    - 新增根檔案系統的 ubinize.cfg 準備步驟
    - 更新 ROOTFS_UBI_CMD 以包含檔案安裝與替換
    - 加入清理步驟以移除生成的 ubinize.cfg
    
    Overall impact and purpose:
    此變更旨在確保 ubifs 配置檔案的正確路徑，並在
    構建過程中自動準備和清理相關檔案，以提高構建的
    一致性和可靠性。
    
    Signed-off-by: yicheng <l5977951@gmail.com>

--- KSZ/Atmel_SOC_SAMA5D3/buildroot/fs/ubifs/ubi.mk
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/fs/ubifs/ubi.mk
@@ -17,14 +17,25 @@ ROOTFS_UBI_DEPENDENCIES = rootfs-ubifs
 ifeq ($(BR2_TARGET_ROOTFS_UBI_USE_CUSTOM_CONFIG),y)
 UBINIZE_CONFIG_FILE_PATH = $(call qstrip,$(BR2_TARGET_ROOTFS_UBI_CUSTOM_CONFIG_FILE))
 else
-UBINIZE_CONFIG_FILE_PATH = fs/ubifs/ubinize.cfg
+UBINIZE_CONFIG_FILE_PATH = $(TOPDIR)/fs/ubifs/ubinize.cfg
 endif
 
+# Prepare ubinize config before fakeroot
+$(BINARIES_DIR)/rootfs.ubi: $(BUILD_DIR)/ubinize.cfg
+
+$(BUILD_DIR)/ubinize.cfg: $(UBINIZE_CONFIG_FILE_PATH)
+	$(INSTALL) -m 0644 $(UBINIZE_CONFIG_FILE_PATH) $@
+	$(SED) 's;BR2_ROOTFS_UBIFS_PATH;$(BINARIES_DIR)/rootfs.ubifs;' $@
+
 define ROOTFS_UBI_CMD
-	$(INSTALL) -m 0644 $(UBINIZE_CONFIG_FILE_PATH) $(BUILD_DIR)/ubinize.cfg
-	$(SED) 's;BR2_ROOTFS_UBIFS_PATH;$@fs;' $(BUILD_DIR)/ubinize.cfg
 	$(HOST_DIR)/usr/sbin/ubinize -o $@ $(UBI_UBINIZE_OPTS) $(BUILD_DIR)/ubinize.cfg
-	rm $(BUILD_DIR)/ubinize.cfg
+endef
+
+# Clean up after build
+ROOTFS_UBI_POST_GEN_HOOKS += ROOTFS_UBI_CLEANUP
+
+define ROOTFS_UBI_CLEANUP
+	rm -f $(BUILD_DIR)/ubinize.cfg
 endef
 
 $(eval $(call ROOTFS_TARGET,ubi))
